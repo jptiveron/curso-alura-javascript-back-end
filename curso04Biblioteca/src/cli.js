a3600e6bc38c30ce5b587c1905eb47c757ba3a2c
João Paulo Tiveron <jptiveron@hotmail.com>
2025-04-28 17:35:11 -0300

    adicionados arquivos do curso sobre bibliotecas node

---
+++ curso04Biblioteca/src/cli.js
@@ -0,0 +1,76 @@
+import fs from "fs";
+import path from "path";
+import trataErros from "./erros/funcoesErro.js";
+import { contaPalavras } from "./index.js";
+import { montaSaidaArquivo } from "./helpers.js";
+import { Command } from "commander";
+import chalk from "chalk";
+
+const program = new Command();
+
+program
+  .version("0.0.1")
+  .option("-t, --texto <string>", "caminho do texto a ser processado")
+  .option("-d, --destino <string>", "caminho do arquivo de resultado")
+  .action((options) => {
+    const { texto, destino } = options;
+    if (!texto || !destino) {
+      console.error(
+        chalk.red("Por favor, forneça os caminhos de texto e destino.")
+      );
+      program.help();
+      return;
+    }
+
+    const caminhoTexto = path.resolve(texto);
+    const caminhoDestino = path.resolve(destino);
+
+    try {
+      processaArquivo(caminhoTexto, caminhoDestino);
+      console.log(chalk.green("Texto processado com sucesso."));
+    } catch (erro) {
+      console.log("Erro ao processar o arquivo:", erro);
+    }
+  });
+
+program.parse();
+
+function processaArquivo(texto, destino) {
+  fs.readFile(texto, "utf-8", (erro, texto) => {
+    try {
+      if (erro) throw erro;
+      const resultado = contaPalavras(texto);
+      criaESalvaArquivo(resultado, destino);
+    } catch (erro) {
+      trataErros(erro);
+    }
+  });
+}
+
+async function criaESalvaArquivo(listaPalavras, endereco) {
+  const arquivoNovo = `${endereco}/resultado.txt`;
+  const textoPalavras = montaSaidaArquivo(listaPalavras);
+  try {
+    await fs.promises.writeFile(arquivoNovo, textoPalavras);
+    console.log("Arquivo criado com sucesso");
+  } catch (erro) {
+    throw erro;
+  }
+}
+
+// function criaESalvaArquivo(listaPalavras, endereco) {
+//   const arquivoNovo = `${endereco}/resultado.txt`;
+//   const textoPalavras = JSON.stringify(listaPalavras);
+
+//   fs.promises
+//     .writeFile(arquivoNovo, textoPalavras)
+//     .then(() => {
+//       console.log("Arquivo criado com sucesso");
+//     })
+//     .catch((erro) => {
+//       throw erro;
+//     })
+//     .finally(() => {
+//       console.log("Operação concluída");
+//     });
+// }
